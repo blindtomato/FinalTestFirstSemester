7eb6be64160a56e74bc8dddc9b1a08d2bb38273f
Aleksandra Yudina <yudin.p.s@gmail.com>
2022-11-27 16:18:54 +0700

    feat: add solution of task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿string[] fillArray(int length)
+    {
+        string[] array = new string[length];
+        for(int i = 0; i < length; i++)
+        {
+            Console.WriteLine($"Введите {i + 1} элемент массива");
+            array[i] = Console.ReadLine();
+        }
+        return array;
+    }
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    Console.Write(string.Join(", ", array));
+    Console.WriteLine("]");
+}
+
+int CountElements(string[] array)
+{
+    int result = 0;
+    foreach (string value in array)
+    {
+        if (value.Length <= 3)
+            result++;
+    }
+    return result;
+}
+
+string[] DeleteElements(string[] array)
+{
+    int length = CountElements(array);
+    string[] arrayResult = new string[length];
+    int i = 0;
+    foreach (string value in array)
+    {
+        if (value.Length <= 3)
+        {
+            arrayResult[i++] = value;            
+        }
+    }
+    return arrayResult;
+}
+
+Console.WriteLine("Введите количество элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = fillArray(n);
+Console.WriteLine("Исходный массив");
+PrintArray(array);
+Console.WriteLine("Результирующий массив");
+PrintArray(DeleteElements(array));
